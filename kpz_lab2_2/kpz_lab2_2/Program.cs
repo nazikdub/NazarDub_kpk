f053dcb6b99f674802ac7673b853585a27981fdb
nazikdub <63237651+nazikdub@users.noreply.github.com>
2020-10-27 14:06:36 +0200

    kpz_2

---
+++ kpz_lab2_2/kpz_lab2_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kpz_lab2_2
+{
+    class Airplane
+    {
+        private string StartCity;
+        private string FinishCity;
+        private Date StartDate;
+        private Date FinishDate;
+    }
+
+    class Date
+    {
+        private int Year;
+        private int Month;
+        private int Day;
+        private int Hours;
+        private int Minutes;
+
+        extern Date(int Year = 0, int Month = 0, int Day = 0, int Hours = 0, int Minutes = 0)
+        {
+            this.Year = Year;
+            this.Month = Month;
+            this.Day = Day;
+            this.Hours = Hours;
+            this.Minutes = Minutes;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
